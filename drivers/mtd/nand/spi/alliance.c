dea24cbcd236009ba67c2d656747621adddfbea5
Samuel Huxtable <64793716+Sam-Huxtable@users.noreply.github.com>
2024-10-16 13:48:08 +0100

    Update alliance.c

--- drivers/mtd/nand/spi/alliance.c
+++ drivers/mtd/nand/spi/alliance.c
@@ -86,7 +86,7 @@ static int alliance_ecc_get_status(struct spinand_device *spinand,
 static const struct spinand_info alliance_spinand_table[] = {
 	/* AS5F 1Gb 3.3V */
 	SPINAND_INFO("AS5F31G04SND-08LIN",
-		     SPINAND_ID(SPINAND_READID_METHOD_OPCODE_ADDR, 0x25),
+		     SPINAND_ID(0x25),
 		     NAND_MEMORG(1, 2048, 64, 64, 1024, 20, 1, 1, 1),
 		     NAND_ECCREQ(4, 512),
 		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
@@ -96,7 +96,7 @@ static const struct spinand_info alliance_spinand_table[] = {
 		     SPINAND_ECCINFO(&alliance_ooblayout, alliance_ecc_get_status)),
 	/* AS5F 2Gb 3.3V */
 	SPINAND_INFO("AS5F32G04SND-08LIN",
-		     SPINAND_ID(SPINAND_READID_METHOD_OPCODE_ADDR, 0x2E),
+		     SPINAND_ID(0x2E),
 		     NAND_MEMORG(1, 2048, 128, 64, 2048, 40, 1, 1, 1),
 		     NAND_ECCREQ(8, 512),
 		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
@@ -106,7 +106,7 @@ static const struct spinand_info alliance_spinand_table[] = {
 		     SPINAND_ECCINFO(&alliance_ooblayout, alliance_ecc_get_status)),
 	/* AS5F 2Gb 1.8V */
 	SPINAND_INFO("AS5F12G04SND-10LIN",
-		     SPINAND_ID(SPINAND_READID_METHOD_OPCODE_ADDR, 0x8E),
+		     SPINAND_ID(0x8E),
 		     NAND_MEMORG(1, 2048, 128, 64, 2048, 40, 1, 1, 1),
 		     NAND_ECCREQ(8, 512),
 		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
@@ -116,7 +116,7 @@ static const struct spinand_info alliance_spinand_table[] = {
 		     SPINAND_ECCINFO(&alliance_ooblayout, alliance_ecc_get_status)),
 	/* AS5F 4Gb 3.3V */
 	SPINAND_INFO("AS5F34G04SND-08LIN",
-		     SPINAND_ID(SPINAND_READID_METHOD_OPCODE_ADDR, 0x2F),
+		     SPINAND_ID(0x2F),
 		     NAND_MEMORG(1, 2048, 128, 64, 4096, 80, 1, 1, 1),
 		     NAND_ECCREQ(8, 512),
 		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
@@ -126,7 +126,7 @@ static const struct spinand_info alliance_spinand_table[] = {
 		     SPINAND_ECCINFO(&alliance_ooblayout, alliance_ecc_get_status)),
 	/* AS5F 4Gb 1.8V */			     
 	SPINAND_INFO("AS5F14G04SND-10LIN",
-		     SPINAND_ID(SPINAND_READID_METHOD_OPCODE_ADDR, 0x8F),
+		     SPINAND_ID(0x8F),
 		     NAND_MEMORG(1, 2048, 128, 64, 4096, 80, 1, 1, 1),
 		     NAND_ECCREQ(8, 512),
 		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
@@ -136,7 +136,7 @@ static const struct spinand_info alliance_spinand_table[] = {
 		     SPINAND_ECCINFO(&alliance_ooblayout, alliance_ecc_get_status)),
 	/* AS5F 8Gb 3.3V */
 	SPINAND_INFO("AS5F38G04SND-08LIN",
-		     SPINAND_ID(SPINAND_READID_METHOD_OPCODE_DUMMY, 0x2D),
+		     SPINAND_ID(0x2D),
 		     NAND_MEMORG(1, 4096, 256, 64, 4096, 80, 1, 1, 1),
 		     NAND_ECCREQ(8, 512),
 		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
@@ -146,7 +146,7 @@ static const struct spinand_info alliance_spinand_table[] = {
 		     SPINAND_ECCINFO(&alliance_ooblayout, alliance_ecc_get_status)),
 	/* AS5F 8Gb 1.8V */			     
 	SPINAND_INFO("AS5F18G04SND-10LIN",
-		     SPINAND_ID(SPINAND_READID_METHOD_OPCODE_DUMMY, 0x8D),
+		     SPINAND_ID(0x8D),
 		     NAND_MEMORG(1, 4096, 256, 64, 4096, 80, 1, 1, 1),
 		     NAND_ECCREQ(8, 512),
 		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
