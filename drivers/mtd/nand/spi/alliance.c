f14a3dd4c34f17b7e3a2531a4892e2e078dec2e1
Samuel Huxtable <64793716+Sam-Huxtable@users.noreply.github.com>
2024-10-23 16:24:47 +0100

    Update alliance.c

--- drivers/mtd/nand/spi/alliance.c
+++ drivers/mtd/nand/spi/alliance.c
@@ -171,7 +171,7 @@ static int alliance_spianand_detect(struct spinand_device *spinand)
 
 	ret = spinand_match_and_init(spinand, alliance_spinand_table,
 				     ARRAY_SIZE(alliance_spinand_table),
-				     id[1]);
+				     &id[1]);
 	if (ret)
 		return ret;
 
