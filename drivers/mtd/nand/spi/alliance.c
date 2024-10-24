b948b0540b35ca17a5c213cf884e0e7355f4b6db
Samuel Huxtable <64793716+Sam-Huxtable@users.noreply.github.com>
2024-10-24 12:03:53 +0100

    Update alliance.c

--- drivers/mtd/nand/spi/alliance.c
+++ drivers/mtd/nand/spi/alliance.c
@@ -161,7 +161,7 @@ static int alliance_spianand_detect(struct spinand_device *spinand)
 {
 	u8 *id = spinand->id.data;
 	int ret;
-	printf("Alliance");
+	printf("Alliance,");
 	/*
 	 * Macronix SPI NAND read ID needs a dummy byte, so the first byte in
 	 * raw_id is garbage.
