aba1ca6c43dc41d25cd20622a1091fb78b880089
Samuel Huxtable <64793716+Sam-Huxtable@users.noreply.github.com>
2024-10-24 10:47:16 +0100

    Update pc805_spl.c

--- board/picocom/pc805/pc805_spl.c
+++ board/picocom/pc805/pc805_spl.c
@@ -24,7 +24,7 @@ void board_spinand_init(void)
 {
 	struct udevice *dev;
 	int ret;
-	printf("pc805_spl.c");
+	printf("pc805_spl.c: %s/n", UCLASS_MTD);
 	ret = uclass_get_device_by_driver(UCLASS_MTD,
 					  DM_GET_DRIVER(spinand), &dev);
 	if (ret && ret != -ENODEV) {
