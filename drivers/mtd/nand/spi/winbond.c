4283411f93e3aa80f32fcbc873a647899e2682c9
Samuel Huxtable <64793716+Sam-Huxtable@users.noreply.github.com>
2024-10-25 13:19:39 +0100

    Update winbond.c

--- drivers/mtd/nand/spi/winbond.c
+++ drivers/mtd/nand/spi/winbond.c
@@ -18,7 +18,63 @@
 #define SPINAND_MFR_WINBOND		0x52
 
 #define WINBOND_CFG_BUF_READ		BIT(3)
+#define SPINAND_MFR_ALLIANCE		0x5
 
+#define STATUS_ECC_LIMIT_BITFLIPS (3 << 4)
+
+static int alliance_ooblayout_ecc(struct mtd_info *mtd, int section,
+				  struct mtd_oob_region *region)
+{
+	if (section)
+		return -ERANGE;
+
+	region->offset = mtd->oobsize / 2;
+	region->length = mtd->oobsize / 2;
+
+	return 0;
+}
+
+static int alliance_ooblayout_free(struct mtd_info *mtd, int section,
+				   struct mtd_oob_region *region)
+{
+	if (section)
+		return -ERANGE;
+
+	/* Reserve 1 bytes for the BBM. */
+	region->offset = 1;
+	region->length = (mtd->oobsize / 2) - 1;
+
+	return 0;
+}
+
+static const struct mtd_ooblayout_ops alliance_ooblayout = {
+	.ecc = alliance_ooblayout_ecc,
+	.rfree = alliance_ooblayout_free,
+};
+
+static int alliance_ecc_get_status(struct spinand_device *spinand,
+				   u8 status)
+{
+	switch (status & STATUS_ECC_MASK) {
+	case STATUS_ECC_NO_BITFLIPS:
+		return 0;
+
+	case STATUS_ECC_UNCOR_ERROR:
+		return -EBADMSG;
+
+	case STATUS_ECC_HAS_BITFLIPS:
+		return 1;
+
+	case STATUS_ECC_LIMIT_BITFLIPS:
+		return 3;
+
+
+	default:
+		break;
+	}
+
+	return -EINVAL;
+}
 static SPINAND_OP_VARIANTS(read_cache_variants,
 		SPINAND_PAGE_READ_FROM_CACHE_QUADIO_OP(0, 2, NULL, 0),
 		SPINAND_PAGE_READ_FROM_CACHE_X4_OP(0, 1, NULL, 0),
@@ -108,15 +164,14 @@ static int w25m02gv_select_target(struct spinand_device *spinand,
 }
 
 static const struct spinand_info winbond_spinand_table[] = {
-	SPINAND_INFO("W25M02GV", 0x8D,
-		     NAND_MEMORG(1, 2048, 64, 64, 1024, 1, 1, 2),
-		     NAND_ECCREQ(1, 512),
+	SPINAND_INFO("AS5F18G04SND-10LIN", 0x8D,
+		     NAND_MEMORG(1, 4096, 256, 64, 4096, 1, 1, 1),
+		     NAND_ECCREQ(8, 512),
 		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
 					      &write_cache_variants,
 					      &update_cache_variants),
 		     0,
-		     SPINAND_ECCINFO(&w25m02gv_ooblayout, NULL),
-		     SPINAND_SELECT_TARGET(w25m02gv_select_target)),
+		     SPINAND_ECCINFO(&alliance_ooblayout, alliance_ecc_get_status)),
 	SPINAND_INFO("W25N01GV", 0xAA,
 		     NAND_MEMORG(1, 2048, 64, 64, 1024, 1, 1, 1),
 		     NAND_ECCREQ(1, 512),
@@ -192,7 +247,7 @@ static const struct spinand_manufacturer_ops winbond_spinand_manuf_ops = {
 };
 
 const struct spinand_manufacturer alliance_spinand_manufacturer = {
-	.id = SPINAND_MFR_WINBOND,
-	.name = "Winbond",
+	.id = SPINAND_MFR_ALLIANCE,
+	.name = "Alliance",
 	.ops = &winbond_spinand_manuf_ops,
 };
