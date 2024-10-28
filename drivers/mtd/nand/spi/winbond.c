1729cd0c0e381723d31185da02df46754b4fda80
Samuel Huxtable <64793716+Sam-Huxtable@users.noreply.github.com>
2024-10-28 10:34:25 +0000

    Update winbond.c

--- drivers/mtd/nand/spi/winbond.c
+++ drivers/mtd/nand/spi/winbond.c
@@ -15,7 +15,7 @@
 #include <linux/bitops.h>
 #include <linux/mtd/spinand.h>
 
-#define SPINAND_MFR_WINBOND		0x52
+#define SPINAND_MFR_WINBOND		0xEF
 
 #define WINBOND_CFG_BUF_READ		BIT(3)
 #define SPINAND_MFR_ALLIANCE		0x5
