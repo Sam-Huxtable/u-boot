1e9e5efcc027acafa3af5996961ff3533b81336b
Samuel Huxtable <64793716+Sam-Huxtable@users.noreply.github.com>
2024-10-28 10:08:50 +0000

    Update alliance.c

--- drivers/mtd/nand/spi/alliance.c
+++ drivers/mtd/nand/spi/alliance.c
@@ -121,7 +121,7 @@ static const struct spinand_manufacturer_ops alliance_spinand_manuf_ops = {
 	.detect = alliance_spianand_detect,
 };
 
-const struct spinand_manufacturer winbond_spinand_manufacturer = {
+const struct spinand_manufacturer alliance_spinand_manufacturer = {
 	.id = SPINAND_MFR_ALLIANCE,
 	.name = "Alliance",
 	.ops = &alliance_spinand_manuf_ops,
